1ebeafd65ca3b7d679575a6960167a490430f2ad
sgherro <simoferrari96@outlook.it>
2021-01-13 08:45:48 +0100

    - new db materials - fixed bugs

--- honningbrew_wwebsite/src/components/AddBarrel.js
+++ honningbrew_wwebsite/src/components/AddBarrel.js
@@ -36,7 +36,7 @@ export class AddBarrel extends Component {
     var set_name = this.props.match.id
     var number_battery = set_name.toString().charCodeAt(0) - 64
     var body_instance = {}
-    if (this.state.typeWood !== "" && this.state.capacity > 0 && this.state.acidity > 1.0) {
+    if (this.state.typeWood !== "" && this.state.capacity > 0 && this.state.acidity >= 1.0) {
       body_instance = {
         'type_wood': this.state.typeWood,
         'capacity': this.state.capacity,
@@ -50,13 +50,13 @@ export class AddBarrel extends Component {
           'battery': number_battery,
         }
       }
-      if (this.state.capacity >= 0) {
+      if (this.state.capacity > 0) {
         body_instance = {
           'capacity': this.state.capacity,
           'battery': number_battery,
         }
       }
-      if (this.state.acidity > 1.0) {
+      if (this.state.acidity >= 1.0) {
         body_instance = {
           'acidity': this.state.acidity,
           'battery': this.props.match.id,
